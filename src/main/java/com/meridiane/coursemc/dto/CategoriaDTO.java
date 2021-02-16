9bb03338f7e7f65251cc56bc20a5fffd1a6436f3
meridiane <gonkaschessoff@gmail.com>
2021-02-16 20:21:11 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/meridiane/coursemc/dto/CategoriaDTO.java
+++ src/main/java/com/meridiane/coursemc/dto/CategoriaDTO.java
@@ -2,11 +2,19 @@ package com.meridiane.coursemc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.meridiane.coursemc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=80, message="Tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO() {
--- src/main/java/com/meridiane/coursemc/services/CategoriaService.java
+++ src/main/java/com/meridiane/coursemc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.meridiane.coursemc.domain.Categoria;
+import com.meridiane.coursemc.dto.CategoriaDTO;
 import com.meridiane.coursemc.repositories.CategoriaRepository;
 import com.meridiane.coursemc.services.exceptions.DataIntegrityException;
 import com.meridiane.coursemc.services.exceptions.ObjectNotFoundException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO (CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
