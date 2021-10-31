ef4a04b4d782a8483d96f0addf39aad32e0d77c6
Merischessoff <gonkaschessoff@gmail.com>
2021-10-31 19:39:21 -0300

    Validação customizada: email não repetido na inserção de cliente

--- src/main/java/com/meridiane/coursemc/repositories/CidadeRepository.java
+++ src/main/java/com/meridiane/coursemc/repositories/CidadeRepository.java
@@ -1,11 +1,14 @@
 package com.meridiane.coursemc.repositories;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.meridiane.coursemc.domain.Cidade;
+import com.meridiane.coursemc.domain.Cliente;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade,Integer>{
-
+	
 }
